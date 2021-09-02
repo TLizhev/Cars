8adbebe5c0a8e643d15286fe695b337bb157566a
TodorLizhev <tlijev@gmail.com>
2021-09-02 13:20:00 +0100

    Add project files.

---
+++ Data/CarsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars.Data
+{
+    public class CarsDbContext : IdentityDbContext
+    {
+        public CarsDbContext(DbContextOptions<CarsDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
