b45dbbacb39f673a12786c3f426976da4d442b39
TodorLizhev <tlijev@gmail.com>
2021-09-07 10:05:10 +0100

    Added new migrations

--- Data/CarsDbContext.cs
+++ Data/CarsDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Cars.Data.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -23,6 +24,18 @@ namespace Cars.Data
                 .HasOne(c => c.Category)
                 .WithMany(c => c.Cars)
                 .HasForeignKey(c => c.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Car>()
+                .HasOne(c => c.Dealer)
+                .WithMany(d => d.Cars)
+                .HasForeignKey(c => c.DealerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Dealer>()
+                .HasOne<IdentityUser>()
+                .WithOne()
+                .HasForeignKey<Dealer>(d => d.UserId)
                 .OnDelete(DeleteBehavior.Restrict);
 
             base.OnModelCreating(builder);
---
+++ Data/Migrations/20210907085545_DealersTable.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cars.Data.Migrations
+{
+    public partial class DealersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DealerId",
+                table: "Cars",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Dealer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserId1 = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dealer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Dealer_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Dealer_AspNetUsers_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_DealerId",
+                table: "Cars",
+                column: "DealerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dealer_UserId",
+                table: "Dealer",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dealer_UserId1",
+                table: "Dealer",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_Dealer_DealerId",
+                table: "Cars",
+                column: "DealerId",
+                principalTable: "Dealer",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_Dealer_DealerId",
+                table: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "Dealer");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_DealerId",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "DealerId",
+                table: "Cars");
+        }
+    }
+}
